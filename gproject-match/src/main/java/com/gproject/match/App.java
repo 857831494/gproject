fa80ccbd1e9593216e261ac6f8d45fcd5f840882
857831494 <857831494@qq.com>
2019-08-22 20:47:53 +0800

    匹配服

--- gproject-match/src/main/java/com/gproject/match/App.java
+++ gproject-match/src/main/java/com/gproject/match/App.java
@@ -1,13 +1,24 @@
 package com.gproject.match;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.gproject.common.config.AppinitHandler;
+import com.gproject.common.utils.IDef;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication()
+@ComponentScan(IDef.ROOT_PACKAGE)
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SpringApplication application = new SpringApplication(App.class);
+        application.addListeners(new AppinitHandler());
+        application.run(args);
     }
 }
