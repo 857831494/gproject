2e5d8ace3ef380e146905b38903eda54ad75f7ac
857831494 <857831494@qq.com>
2019-09-23 20:04:56 +0800

    1

---
+++ gproject-center/res/static/html/index/MenuConfig.js
@@ -0,0 +1,15 @@
+var MenuConfig = 
+[
+    { text: '基础',isexpand:false, children: [ 
+		{url:"Excel/Servers",text:"配置表热更新"},
+		{url:"Excel/ExcelGrid-dev.html",text:"查看配置表"}
+	]
+    },
+    { text: '过滤器', isexpand: false, children: [
+		{ url: "demos/filter/filter.htm", text: "自定义查询" },
+		{ url: "demos/filter/filterwin.htm", text: "在窗口显示" },
+		{ url: "demos/filter/grid.htm", text: "配合表格" } 
+	]
+    }
+
+];
